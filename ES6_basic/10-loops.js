7b819fda6f7c5b2d8ef68f4671e5e12613977eaa
Franco-byte <fd.reyes3110@gmail.com>
2025-03-28 14:35:13 -0300

    No se permite usar .entries() en el array

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
   const copy = [...array];
-  for (const [idx, value] of array.entries()) {
+  let idx = 0;
+  for (const value of array) {
     copy[idx] = appendString + value;
+    idx += 1;
   }
 
   return copy;
