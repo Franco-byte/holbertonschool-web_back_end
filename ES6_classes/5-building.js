d8e91bb77ae5103199c436ddb4ce6cfff972278e
Franco-byte <fd.reyes3110@gmail.com>
2025-03-28 22:38:01 -0300

    Task 5 - We returned

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building || Object.getPrototypeOf(this).evacuationWarningMessage === undefined) {
+    if (new.target === Building || this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this.sqft = sqft;
