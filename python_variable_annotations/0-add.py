6a11779b78b4457459b1fa47a0eff2b12a7f5ac2
Franco-byte <fd.reyes3110@gmail.com>
2025-04-26 14:28:51 -0300

    The first line is correct!

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,9 @@
-#!/usr/bin/env pyhton3
+#!/usr/bin/env python3
+"""
+function add
+"""
+
+
 def add(a: float, b: float):
+    '''Returb the multiplication between two float numbers'''
     return a + b
