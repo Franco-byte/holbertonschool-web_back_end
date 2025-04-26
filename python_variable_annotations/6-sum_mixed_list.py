046b93ef26a6324bb2bd9ef92e02009d4036a65d
Franco-byte <fd.reyes3110@gmail.com>
2025-04-26 16:07:44 -0300

    I forgot to change the "|" to a comma

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -6,7 +6,7 @@ integers and floats and returns their sum as a float.
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[int | float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     # Optional: mxd_lst: List[int | float]
     '''Sum a list of flouts and ints and returns it'''
     return sum(mxd_lst)
