40bdf38d739da6a9090da1b5be9e30bb641faa60
Franco-byte <fd.reyes3110@gmail.com>
2025-04-26 16:02:50 -0300

    Import List from typing and use it

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -5,6 +5,9 @@ of floats as argument and returns their sum as a float.
 '''
 
 
-def sum_list(input_list: list[float]) -> float:
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
     '''Sum a list of flout and returns it'''
     return sum(input_list)
