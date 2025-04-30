06a3d325f8cecd3198b555053ea15f6ddaf4f1d2
Franco-byte <fd.reyes3110@gmail.com>
2025-04-30 11:14:42 -0300

    Fix the errors

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,10 +6,10 @@ More to async
 
 from asyncio import sleep
 from random import uniform
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]: #type: ignore
     '''Generator whit delay'''
     for _ in range(10):
         yield uniform(0, 10)
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -8,13 +8,13 @@ from time import perf_counter
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     '''Using async.gather'''
     start = perf_counter()
     await gather(
-        async_comprehension(), 
-        async_comprehension(), 
-        async_comprehension(), 
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
         async_comprehension()
         )
     end = perf_counter()
