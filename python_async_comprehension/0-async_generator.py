3dc3288fd349c3b318ce309daf5d63ec23e79c91
Franco-byte <fd.reyes3110@gmail.com>
2025-04-30 11:25:30 -0300

    Fix pycodestyle Task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,7 +9,7 @@ from random import uniform
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]: # type: ignore
+async def async_generator() -> Generator[float, None, None]:  # type: ignore
     '''Generator whit delay'''
     for _ in range(10):
         yield uniform(0, 10)
