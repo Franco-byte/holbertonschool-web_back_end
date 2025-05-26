c4e634ce063c6603d679a279d654e8e1543c7754
Franco-byte <fd.reyes3110@gmail.com>
2025-05-25 23:04:39 -0300

    .

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -9,28 +9,27 @@ function countStudents(path) {
     const fields = {};
 
     for (const line of students) {
-        const parts = line.split(',');
-        const firstName = parts[0];
-        const field = parts[3];
+      const parts = line.split(',');
+      const firstName = parts[0];
+      const field = parts[3];
 
-        if (!fields[field]) {
-            fields[field] = [];
-        }
+      if (!fields[field]) {
+          fields[field] = [];
+      }
 
-        fields[field].push(firstName);
+      fields[field].push(firstName);
     }
 
     console.log(`Number of students: ${students.length}`);
     for (const field in fields) {
       if (Object.prototype.hasOwnProperty.call(fields, field)) {
         console.log(
-            `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
-        );
-      };
-    };
+          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
+    }
   } catch (error) {
-      throw new Error('Cannot load the database');
-  };
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
