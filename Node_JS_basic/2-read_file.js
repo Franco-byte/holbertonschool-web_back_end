3b2dbe51dcd7150d888ad759b9fdecdf6aec4742
Franco-byte <fd.reyes3110@gmail.com>
2025-05-25 23:13:50 -0300

    .

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -24,7 +24,7 @@ function countStudents(path) {
     for (const field in fields) {
       if (Object.prototype.hasOwnProperty.call(fields, field)) {
         console.log(
-          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
         );
       }
     }
